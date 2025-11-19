2a511389a75f37b717424fae443e6f7fea0435c1
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 12:41:08 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import os
 from langchain_openai import ChatOpenAI
+import os
 
 def get_llm():
     return ChatOpenAI(
-        model="gpt-4o",     # modelo 100% compatível
+        model="gpt-4o-mini",   # 100% compatível com LangChain
         temperature=0.1,
         api_key=os.getenv("OPENAI_API_KEY")
     )
