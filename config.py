1294a80a8928527e1eb491368502e9433f60f4ac
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 13:09:41 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-from langchain_openai.chat_models import ChatOpenAI
-
+from langchain_openai import ChatOpenAI
 import os
-import langchain_openai
-print("Using langchain_openai from:", langchain_openai.__file__)
 
 def get_llm():
     print("Using ChatOpenAI from module:", ChatOpenAI.__module__)
